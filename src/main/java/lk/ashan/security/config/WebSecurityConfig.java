09b45d1d44eef085038ff2f2bd42efa8e4313293
Ashan Dissanayake <ashanpathum899@gmail.com>
2025-02-15 21:04:50 +0530

    Overrided default security configurations with custom username and password

---
+++ src/main/java/lk/ashan/security/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package lk.ashan.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("Navo")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
