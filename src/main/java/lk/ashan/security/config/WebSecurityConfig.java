3b7b700c4bf23b888c9edf93c2fde11a6aa1b151
Ashan Dissanayake <ashanpathum899@gmail.com>
2025-02-27 09:00:04 +0530

    Create a user entity implementing spring userdetails interface and make inmemoryuserdetails service for providing user credentials and update the websecurityconfig class with those implementations

--- src/main/java/lk/ashan/security/config/WebSecurityConfig.java
+++ src/main/java/lk/ashan/security/config/WebSecurityConfig.java
@@ -1,16 +1,18 @@
 package lk.ashan.security.config;
 
+import lk.ashan.security.entity.User;
+import lk.ashan.security.service.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.List;
+
 
 @Configuration
 public class WebSecurityConfig{
@@ -21,6 +23,27 @@ public class WebSecurityConfig{
         return http.build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails u = new User("navo", "12345", "read");
+        List<UserDetails> users = List.of(u);
+        return new InMemoryUserDetailsService(users);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
+
+
+
+
+
+
+
+/*
     @Bean
     public UserDetailsService userDetailsService() {
 
@@ -35,9 +58,4 @@ public class WebSecurityConfig{
 
         return userDetailsService;
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-}
+*/
